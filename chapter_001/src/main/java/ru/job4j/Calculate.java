473ba89b0649b9c69ef761bb4481afbe64dbb8cc
euginezh <soc.eugine@gmail.com>
2018-01-26 13:27:43 +0600

    1.3. JavaDoc. Code convention

--- chapter_001/src/main/java/ru/job4j/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/Calculate.java
@@ -1,8 +1,19 @@
 package ru.job4j;
 
+/**
+* Calculate.
+ *
+ * @author Yevgeniy Yelyanyushkin (eelyanyushkin@gmail.com)
+ * @version $Id$
+ * @since 0.1
+
+*/
 public class Calculate {
-	public static void main(String[] args) {
+		 /**
+     * Main.
+     * @param args - args.
+     */
+	 	public static void main(String[] args) {
 		System.out.println("Hello world!");
 	}
-
 }	
